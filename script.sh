386f3d5a3cc92be6637d8330171d82a48a26ec3f
Audrey Rasolonjatovo <rasolonjatovo.audrey@gmail.com>
2024-10-01 14:23:35 +0200

    [UPDATE] docker-compose installation on debian

--- script.sh
+++ script.sh
@@ -5,5 +5,5 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debi
 sudo apt update
 apt-cache policy docker-ce
 sudo apt install docker-ce
-sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-%60uname -s-uname -m -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.25.3/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
